4dd193bb4dd404ffb1359b8dc294d5f52d900f1c
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-11-06 17:00:04 +0200

    edit for js

--- js/c01.js
+++ js/c01.js
@@ -10,5 +10,19 @@
  }
 
  document.write('<h2>' +greeting+ '</h2>');
+
+ 
+
+    var showNumber = prompt("What the number of dogs do you have?")
+    var item;
+    if (showNumber >= 10){
+    item = '<img src = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1094874726.png?crop=0.542xw:0.814xh;0.0472xw,0.127xh&resize=640:*">'
+    }
+    else {
+        item = '<img src ="https://i.ytimg.com/vi/OOFGdRmN70k/maxresdefault.jpg">'
+    }
+
+
+document.write('<h2>' +item+ '</h2>');
  
  
